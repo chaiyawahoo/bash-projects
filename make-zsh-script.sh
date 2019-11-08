ffcfd95d8a37d8317e60e285ef7ada73d3c75aa2
chaiyawahoo <chaiyawahoo@gmail.com>
2019-11-08 04:46:20 -0800

    Update make-zsh-script.sh

--- make-zsh-script.sh
+++ make-zsh-script.sh
@@ -2,7 +2,6 @@
 forced=
 file=
 ext=
-user=
 opts=0
 
 if  [[ $1 = "-f" || "--forced" ]]; then
@@ -14,19 +13,10 @@ fi
 
 file=$(echo $@ | cut -d " " -f $(( 1 + opts )))
 ext=$(echo $file | awk -F '.' '{print $NF}')
-user=$(echo $@ | cut -d " " -f $(( 2 + opts )))
+# words=$(echo $@ | wc - w)
 
 if [[ $ext != "sh" ]]; then
     file=$file".sh"
-fi
-
-if [[ -n $user ]]; then
-    if [[ -n $(dscl . list /Users | grep $user) ]]; then
-        chown $user $file
-    else
-        echo "User '$user' does not exist." >&2
-        exit 1
-    fi
 fi
 
 if [[ -f $file && forced = 0 ]]; then
